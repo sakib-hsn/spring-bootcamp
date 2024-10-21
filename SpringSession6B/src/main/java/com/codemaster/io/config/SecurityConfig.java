db8672cd97b1d9512a13f3cede40d73dd6d46d1a
sakib-hsn <hsakib8685@gmail.com>
2024-10-21 15:58:34 +0600

    UserDetailsService add

---
+++ SpringSession6B/src/main/java/com/codemaster/io/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.codemaster.io.config;
+
+import com.codemaster.io.handler.CustomAccessDeniedHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@Component
+@EnableWebSecurity(debug = true)
+//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // Enable method-level security
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        User admin = new User("admin", "1234",
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),
+                        new SimpleGrantedAuthority("ADMIN:READ_PERMISSION")));
+
+        User moderator = new User("moderator", "1234",
+                List.of(new SimpleGrantedAuthority("ROLE_MODERATOR"),
+                        new SimpleGrantedAuthority("MODERATOR:WRITE_PERMISSION")));
+
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager(admin, moderator);
+
+        return inMemoryUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
