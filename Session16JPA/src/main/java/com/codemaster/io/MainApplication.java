3e01bb73e496cea3d19e16976b856613bb06b6d6
sakib <hsakib8685@gmail.com>
2024-11-19 18:30:02 +0600

    JPA add

--- Session16JPA/src/main/java/com/codemaster/io/MainApplication.java
+++ Session16JPA/src/main/java/com/codemaster/io/MainApplication.java
@@ -1,20 +1,68 @@
 package com.codemaster.io;
 
-import java.lang.reflect.Proxy;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+import java.util.Optional;
+
+@SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
-        Car realCar = new RealCar();
+//        Car realCar = new RealCar();
+//
+//        // Interface Car
+//        // Object realCar
+//        // ClassLoader Car
+//
+//        Car proxy = (Car) Proxy.newProxyInstance(Car.class.getClassLoader(),
+//                new Class<?>[]{Car.class}, new LogAdder(realCar));
+//
+//        proxy.startEngine();
+//        proxy.stopEngine();
+//        proxy.makeSound();
 
-        // Interface Car
-        // Object realCar
-        // ClassLoader Car
+        ApplicationContext ctx = SpringApplication.run(MainApplication.class);
+//        JdbcTemplate jdbcTemplate = ctx.getBean(JdbcTemplate.class);
 
-        Car proxy = (Car) Proxy.newProxyInstance(Car.class.getClassLoader(),
-                new Class<?>[]{Car.class}, new LogAdder(realCar));
 
-        proxy.startEngine();
-        proxy.stopEngine();
-        proxy.makeSound();
+        User user = User.builder()
+                .id(1)
+                .name("Admin")
+                .build();
+
+//        UserRepository userRepository = (UserRepository) RepositoryProxy
+//                .getProxyInstance(UserRepository.class, User.class, jdbcTemplate);
+//
+////        userRepository.insert(user);
+//        User user1 = userRepository.getByName("Admin");
+//        System.out.println("user1 = " + user1);
+
+        UserRepository userRepository = ctx.getBean(UserRepository.class);
+
+        userRepository.save(user);
+//        Optional<User> user1 = userRepository.findById(1);
+        Optional<User> user1 = userRepository.findByName("Admin");
+        System.out.println("user1 = " + user1);
+
+
+//        ProductRepository productRepository = (ProductRepository) RepositoryProxy
+//                .getProxyInstance(ProductRepository.class, Product.class, jdbcTemplate);
+
+        Product product = Product.builder()
+                .id(1)
+                .title("iPhone")
+                .build();
+////        productRepository.insert(product);
+//        Product product1 = productRepository.getByTitle("iphone");
+//        System.out.println("product1 = " + product1);
+
+        ProductRepository productRepository = ctx.getBean(ProductRepository.class);
+
+        productRepository.save(product);
+        Optional<Product> product1 = productRepository.findById(1);
+        System.out.println("product1 = " + product1);
+
+
     }
 }
---
+++ Session16JPA/src/main/java/com/codemaster/io/User.java
@@ -0,0 +1,21 @@
+package com.codemaster.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    private int id;
+
+    private String name;
+}
